439dc7d734c018673534ca7b52da5842390a324b
orange <orange666.nice@gmail.com>
2021-09-15 21:31:43 +0800

    Initial commit

---
+++ DataHandle/DbReader.cs
@@ -0,0 +1,50 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+
+namespace DataHandle
+{
+    public class DbReader
+    {
+        private static DbInfo _info;
+
+        private IDbConnection _conn;
+
+        public static DbReader Instance=> Create();
+
+        public DbReader(IDbConnection conn){
+            _conn = conn;
+        }
+
+        public static void Init(DbInfo info)
+        {
+            _info = info;
+        }
+
+        public static DbReader Create()
+        {
+            switch (_info.Type)
+            {
+                case DbType.MSSql:
+                    return new DbReader(new SqlConnection(_info.ConString));
+                case DbType.Mysql:
+                    return new DbReader(new MySqlConnection(_info.ConString));
+                default:
+                    throw new ArgumentException("暂不支持该种数据库");
+            }
+        }
+
+        public IEnumerable<T> Query<T>(string sql)
+        {
+            return _conn.Query<T>(sql);
+        }
+
+    }
+}
---
+++ WebTestDemo/Controllers/DataController.cs
@@ -0,0 +1,24 @@
+﻿using DataHandle;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebTestDemo.Models;
+
+namespace WebTestDemo.Controllers
+{
+    [ApiController]
+    [Route("api/data")]
+    public class DataController:ControllerBase
+    {
+        private DbReader dbReader = DbReader.Instance;
+        
+        [HttpGet]
+        public IActionResult Get()
+        {
+            string sql = "select Users.* from Users";
+            return new JsonResult(dbReader.Query<User>(sql));
+        }
+    }
+}
